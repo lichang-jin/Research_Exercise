854b1195464abe5c926b2693f42b8eb1cb6e2e34
lichang-jin <3517179979@qq.com>
2025-12-09 09:15:32 +0800

    update

--- VGGT/vggt/utils/helper.py
+++ VGGT/vggt/utils/helper.py
@@ -2,9 +2,12 @@ import numpy as np
 
 
 def randomly_limit_trues(mask: np.ndarray, max_trues: int) -> np.ndarray:
-    """
-    If mask has more than max_trues True values, randomly keep only max_trues of them and set the rest to False.
-    :param mask:
-    :param max_trues:
-    :return:
-    """
+    """If mask has more than max_trues True values, randomly keep only max_trues of them and set the rest to False."""
+    true_index = np.flatnonzero(mask)
+    if len(true_index) <= max_trues:
+        return mask
+
+    sampled_index = np.random.choice(true_index, size=max_trues, replace=False)
+    limit_flat_mask = np.zeros(mask.size, dtype=bool)
+    limit_flat_mask[sampled_index] = True
+    return limit_flat_mask.reshape(mask.shape)
